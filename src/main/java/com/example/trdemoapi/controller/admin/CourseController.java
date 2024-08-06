a64b01908d3d542fc2a1061d62f5c064f8815285
Szobonya Dávid <szobonya.david@gmail.com>
2024-08-06 19:09:56 +0200

    course controller

--- src/main/java/com/example/trdemoapi/controller/admin/CourseController.java
+++ src/main/java/com/example/trdemoapi/controller/admin/CourseController.java
@@ -1,7 +1,11 @@
 package com.example.trdemoapi.controller.admin;
 
+import com.example.trdemoapi.dto.UpdateCourseReq;
 import com.example.trdemoapi.model.Course;
+import com.example.trdemoapi.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,18 +15,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/courses")
 @Tag(name="Course", description="the endpoints for authenticated admins to manage courses")
 public class CourseController {
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getCourseById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(new Course());
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateCourse(@PathVariable Long id, @RequestBody Course course) {
+    @Operation(summary="Get course by id", description="Returns with the details of the course with the given id")
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Long id) {
+        var course = courseService.loadCourseById(id);
         return ResponseEntity.ok().body(course);
     }
 
+    @Operation(summary="Update course", description="Returns with the updated course")
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCourse(@PathVariable Long id, @Valid @RequestBody UpdateCourseReq request) {
+        var course = courseService.updateCourse(id, request);
+        return ResponseEntity.ok().body(course);
+    }
+
+    @Operation(summary="Delete course", description="Deletes the course with the given id")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCourse(@PathVariable Long id) {
-        return ResponseEntity.ok().body("delete course");
+        var subject = courseService.loadCourseById(id);
+        courseService.deleteCourse(subject);
+
+        return ResponseEntity.ok().body("Course deleted successfully");
     }
 }
--- src/main/java/com/example/trdemoapi/service/CourseService.java
+++ src/main/java/com/example/trdemoapi/service/CourseService.java
@@ -1,6 +1,7 @@
 package com.example.trdemoapi.service;
 
 import com.example.trdemoapi.dto.CreateCourseReq;
+import com.example.trdemoapi.dto.UpdateCourseReq;
 import com.example.trdemoapi.model.Course;
 import com.example.trdemoapi.model.ERole;
 import com.example.trdemoapi.model.Subject;
@@ -19,6 +20,10 @@ public class CourseService {
         this.userService = userService;
     }
 
+    public Course loadCourseById(Long id) {
+        return courseRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Course not found."));
+    }
+
     @Transactional
     public Course createCourse(Subject subject, CreateCourseReq request) {
         var teacher = userService.loadUserById(request.getTeacherId());
@@ -28,6 +33,25 @@ public class CourseService {
                 .setTeacher(teacher);
 
         userService.addUserRole(teacher, ERole.TEACHER);
+        return courseRepository.save(course);
+    }
+
+    @Transactional
+    public void deleteCourse(Course course) {
+        courseRepository.delete(course);
+    }
+
+    @Transactional
+    public Course updateCourse(Long id, UpdateCourseReq request) {
+        var course = courseRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Course not found."));
+
+        if (request.getCapacity() != null) course.setCapacity(request.getCapacity());
+        if (request.getTeacherId() != null) {
+            var teacher = userService.loadUserById(request.getTeacherId());
+            course.setTeacher(teacher);
+            userService.addUserRole(teacher, ERole.TEACHER);
+        }
+
         return courseRepository.save(course);
     }
 }
